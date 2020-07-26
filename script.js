9d7fcd508169dec5281eaa0d5cf7700542fc6e19
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-07-25 19:13:03 -0700

    Added in space for weekly forecast

--- script.js
+++ script.js
@@ -13,60 +13,60 @@ today = mm + '/' + dd + '/' + yyyy;
 const apiKey = "4d8fb5b93d4af21d66a2948710284366";
 
 form.addEventListener("submit", e => {
-  e.preventDefault();
-  let inputVal = input.value;
+    e.preventDefault();
+    let inputVal = input.value;
 
-  //check if there's already a city
-  const listItems = list.querySelectorAll(".ajax-section .city");
-  const listItemsArray = Array.from(listItems);
+    //check if there's already a city
+    const listItems = list.querySelectorAll(".ajax-section .city");
+    const listItemsArray = Array.from(listItems);
 
-  if (listItemsArray.length > 0) {
-    const filteredArray = listItemsArray.filter(el => {
-      let content = "";
-      //athens,gr
-      if (inputVal.includes(",")) {
-        //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
-        if (inputVal.split(",")[1].length > 2) {
-          inputVal = inputVal.split(",")[0];
-          content = el
-            .querySelector(".city-name span")
-            .textContent.toLowerCase();
-        } else {
-          content = el.querySelector(".city-name").dataset.name.toLowerCase();
+    if (listItemsArray.length > 0) {
+        const filteredArray = listItemsArray.filter(el => {
+            let content = "";
+            //athens,gr
+            if (inputVal.includes(",")) {
+                //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
+                if (inputVal.split(",")[1].length > 2) {
+                    inputVal = inputVal.split(",")[0];
+                    content = el
+                        .querySelector(".city-name span")
+                        .textContent.toLowerCase();
+                } else {
+                    content = el.querySelector(".city-name").dataset.name.toLowerCase();
+                }
+            } else {
+                //athens
+                content = el.querySelector(".city-name span").textContent.toLowerCase();
+            }
+            return content == inputVal.toLowerCase();
+        });
+
+        if (filteredArray.length > 0) {
+            msg.textContent = `You already know the weather for ${
+                filteredArray[0].querySelector(".city-name span").textContent
+                } `;
+            form.reset();
+            input.focus();
+            return;
         }
-      } else {
-        //athens
-        content = el.querySelector(".city-name span").textContent.toLowerCase();
-      }
-      return content == inputVal.toLowerCase();
-    });
-
-    if (filteredArray.length > 0) {
-      msg.textContent = `You already know the weather for ${
-        filteredArray[0].querySelector(".city-name span").textContent
-      } `;
-      form.reset();
-      input.focus();
-      return;
     }
-  }
 
+    function getweather(data) {
+        //ajax here
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 
-  //ajax here
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
+                const { main, name, sys, weather, wind } = data;
+                const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+                    weather[0]["icon"]
+                    }.svg`;
 
-      const { main, name, sys, weather, wind } = data;
-      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
-        weather[0]["icon"]
-      }.svg`;
-
-      const li = document.createElement("li");
-      li.classList.add("city");
-      const markup = `
+                const li = document.createElement("li");
+                li.classList.add("city");
+                const markup = `
         <h2 class="city-name" data-name="${name},${sys.country}">
           <span>${name}</span>
           <sup>${sys.country}</sup>
@@ -79,23 +79,50 @@ form.addEventListener("submit", e => {
 
         <figure>
           <img class="city-icon" src="${icon}" alt="${
-        weather[0]["description"]
-        
-        
-      }">
+                    weather[0]["description"]
+
+
+                    }">
           <figcaption>${weather[0]["description"]}</figcaption>
         </figure>
       `;
-      li.innerHTML = markup;
-      list.appendChild(li);
-    })
-    .catch(() => {
-      msg.textContent = "Please search for a valid city";
-    });
+                li.innerHTML = markup;
+                list.appendChild(li);
+            })
+            .catch(() => {
+                msg.textContent = "Please search for a valid city";
+            });
+    };
+
+    getweather()
+
+    function fiveDayForecast(data) {
+
+        const url = `https://api.openweathermap.org/data/2.5/forecast?q=${inputVal}&appid=${apiKey}&units=metric`
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+
+                const { main, name, sys, weather, wind } = data;
+                console.log("DATA: ", data);
+                console.log("data.list[0].wind.speed: ", data.list[0].wind.speed);
+                console.log("wind.speed: ", wind.speed);
+
+                for (var i = 0; i < data.list.length; i++) {
+                    if (data.list[i].dt_txt.includes("12:00:00")) {
+                        //append the various html and information returned from the data object such as dataInfo.weather[0].description
+                    };
+                };
+            });
+        };
 
     
+    
 
-  msg.textContent = "";
-  form.reset();
-  input.focus();
+
+fiveDayForecast()
+
+msg.textContent = "";
+form.reset();
+input.focus();
 });
