71da817cb794f83c927ca4b168884a2b16b8a0f2
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-07-26 18:52:39 -0700

    Updated local storage logic to correctly store UV color

--- script.js
+++ script.js
@@ -16,12 +16,12 @@ const retrievedData = JSON.parse(localStorage.getItem('cardDataArray')) || [];
 for (let i = 0; i < retrievedData.length; i++) {
     const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
         retrievedData[i].weather[0]["icon"]
-                    }.svg`;
+        }.svg`;
 
-                    console.log(cardDataArray);
+    console.log(cardDataArray);
     const li = document.createElement("li");
-                li.classList.add("city");
-                const markup = `
+    li.classList.add("city");
+    const markup = `
         <h2 class="city-name" data-name="${retrievedData[i].name},${retrievedData[i].sys.country}">
           <span>${retrievedData[i].name}</span>
           <sup>${retrievedData[i].sys.country}</sup>
@@ -30,31 +30,32 @@ for (let i = 0; i < retrievedData.length; i++) {
         <div class="city-temp">${Math.round((retrievedData[i].main.temp * 1.80 + 32))}<sup>°F</sup></div>
         <div class="wind-speed">${retrievedData[i].wind.speed}<sup> MPH Wind Speed</sup></div>
         <div class="wind-speed">${retrievedData[i].main.humidity}<sup> % Humidity</sup></div>
-        <div id = "uv">${retrievedData[i].uv}<sup> UV Index</sup></div>
+        <div id = "uv" >${retrievedData[i].uv}<sup> UV Index</sup></div>
         <figure>
           <img class="city-icon" src="${icon}" alt="${
-            retrievedData[i].weather[0]["description"]
+        retrievedData[i].weather[0]["description"]
 
 
-                    }">
+        }">
           <figcaption>${retrievedData[i].weather[0]["description"]}</figcaption>
         </figure>
       `;
-                li.innerHTML = markup;
-                list.prepend(li);
+    li.innerHTML = markup;
+    list.prepend(li);
 
-                var element = document.getElementById("uv")
-                if (uv <= 4){
-                    element.classList.add("wind-speedgreen")
-                } else if (uv <= 10){
-                    element.classList.add("wind-speedyellow")
-                } else {
-                    element.classList.add("wind-speedred")
-          
-                }
-                
+    var element = document.getElementById("uv")
+    if (retrievedData[i].uv <= 4) {
+        element.classList.add("wind-speedgreen")
+    } else if (retrievedData[i].uv <= 9) {
+        element.classList.add("wind-speedyellow")
+    } else {
+        element.classList.add("wind-speedred")
+
+    }
+
     cardDataArray.push(retrievedData[i])
 }
+
 
 
 //listener for button
@@ -94,57 +95,58 @@ form.addEventListener("submit", e => {
 
     //current date api call 
     function getweather(data) {
-       
+console.log("anything");
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
         fetch(url)
             .then(response => response.json())
             .then(data => {
                 const { main, name, sys, weather, wind, coord } = data;
-               
-                    fetch("http://api.openweathermap.org/data/2.5/uvi?appid=4d8fb5b93d4af21d66a2948710284366&lat=" + data.coord.lat + "&lon=" + data.coord.lon)
+
+                fetch("http://api.openweathermap.org/data/2.5/uvi?appid=4d8fb5b93d4af21d66a2948710284366&lat=" + data.coord.lat + "&lon=" + data.coord.lon)
                     .then(response => response.json())
-                    .then(data => {console.log(data.value)
+                    .then(data => {
+                        console.log(data.value)
                         var uv = data.value;
 
-                    
-                console.log(data); 
-                console.log(uv);
-                const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
-                    weather[0]["icon"]
-                    }.svg`;
-                const li = document.createElement("li");
-                li.classList.add("city");
-                const markup = `
-        <h2 class="city-name" data-name="${name},${sys.country}">
-          <span>${name}</span>
-          <sup>${sys.country}</sup>
-        </h2>
-        <div class="date-div">${today}</div>
-        <div class="city-temp">${Math.round((main.temp * 1.80 + 32))}<sup>°F</sup></div>
-        <div class="wind-speed">${wind.speed}<sup> MPH Wind Speed</sup></div>
-        <div class="wind-speed">${main.humidity}<sup> % Humidity</sup></div>
-        <div id = "uv">${uv}<sup> UV Index</sup></div>
-        <figure>
-          <img class="city-icon" src="${icon}" alt="${
-                    weather[0]["description"]
-                    }">
-          <figcaption>${weather[0]["description"]}</figcaption>
-        </figure>
+
+                        console.log(data);
+                        console.log(uv);
+                        const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+                            weather[0]["icon"]
+                            }.svg`;
+                        const li = document.createElement("li");
+                        li.classList.add("city");
+                        const markup = `
+                            <h2 class="city-name" data-name="${name},${sys.country}">
+                            <span>${name}</span>
+                            <sup>${sys.country}</sup>
+                            </h2>
+                            <div class="date-div">${today}</div>
+                            <div class="city-temp">${Math.round((main.temp * 1.80 + 32))}<sup>°F</sup></div>
+                            <div class="wind-speed">${wind.speed}<sup> MPH Wind Speed</sup></div>
+                            <div class="wind-speed">${main.humidity}<sup> % Humidity</sup></div>
+                            <div id = "uv">${uv}<sup> UV Index</sup></div>
+                            <figure>
+                            <img class="city-icon" src="${icon}" alt="${
+                                                weather[0]["description"]
+                                                }">
+                            <figcaption>${weather[0]["description"]}</figcaption>
+                            </figure>
       `;
-                li.innerHTML = markup;
-                list.prepend(li);
-                var element = document.getElementById("uv")
-                if (uv <= 4){
-                    element.classList.add("wind-speedgreen")
-                } else if (uv <= 10){
-                    element.classList.add("wind-speedyellow")
-                } else {
-                    element.classList.add("wind-speedred")
-          
-                }
-                cardDataArray.push({ main, name, sys, weather, wind, uv, element })
-                localStorage.setItem('cardDataArray', JSON.stringify(cardDataArray))
-                })
+                        li.innerHTML = markup;
+                        list.prepend(li);
+                        var element = document.getElementById("uv")
+                        if (uv <= 4) {
+                            element.classList.add("wind-speedgreen")
+                        } else if (uv <= 9) {
+                            element.classList.add("wind-speedyellow")
+                        } else {
+                            element.classList.add("wind-speedred")
+
+                        }
+                        cardDataArray.push({ main, name, sys, weather, wind, uv, element })
+                        localStorage.setItem('cardDataArray', JSON.stringify(cardDataArray))
+                    })
             })
             .catch(() => {
                 msg.textContent = "Please search for a valid city";
@@ -152,21 +154,21 @@ form.addEventListener("submit", e => {
     };
     getweather()
 
-// weekly forecast api call
-   function fiveDayForecast(data) {
+    // weekly forecast api call
+    function fiveDayForecast(data) {
         const url = `https://api.openweathermap.org/data/2.5/forecast?q=${inputVal}&appid=${apiKey}&units=metric`
         fetch(url)
             .then(response => response.json())
             .then(data => {
-           
-  
+
+
                 for (let i = 0; i < data.list.length; i++) {
                     if (data.list[i].dt_txt.includes("12:00:00")) {
                         const div = document.createElement("div");
                         div.className = 'block';
                         div.classList.add("weekforecast2");
-                        const uvURL =`https://api.openweathermap.org/data/2.5/uvi/forecast?&units=imperial&appid=${apiKey}&lat=${data.city.coord.lat}&lon=${data.city.coord.lon}`
-          
+                        const uvURL = `https://api.openweathermap.org/data/2.5/uvi/forecast?&units=imperial&appid=${apiKey}&lat=${data.city.coord.lat}&lon=${data.city.coord.lon}`
+
                         const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${data.list[i].weather[0].icon}.svg`;
                         const markup = `
                   <div class="forecast5">${data.city.name}</div>
@@ -189,7 +191,7 @@ form.addEventListener("submit", e => {
                         list2.prepend(div);
 
 
-                        };
+                    };
                 };
             });
     };
